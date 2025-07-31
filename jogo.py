85751ba49f37c5663044955f797f837adf914763
gustavo244524 <gustavo.pupo.pereira@escola.pr.gov.br>
2025-07-31 12:18:03 +0000

    primeira parte

---
+++ jogo.py
@@ -0,0 +1,10 @@
+import random
+print("############################")
+print("#    jogo de adivihação    #")
+print("#          gustavo         #")
+print("############################")
+print("seja bem vindo ao game 👾💤 ")
+
+numeroSecreto= random.radrange(0,100)
+totaltentativas = 0
+pontos = 100
